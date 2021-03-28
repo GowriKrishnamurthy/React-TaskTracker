96f049337d9b725ec0fd26309fd33a376b15e911
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2021-03-28 21:37:28 +1100

    useEffect Hook & Fetch tasks from server, Add and Delete tasks on server

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,10 +1,15 @@
 import Task from "./Task";
 
-export const Tasks = ({ tasks, onDelete, onToggle}) => {
+export const Tasks = ({ tasks, onDelete, onToggle }) => {
   return (
     <>
-      {tasks.map((task) => (
-        <Task key={task.id} task={task} onDelete={onDelete} onToggle={onToggle} />
+      {tasks.map((task, index) => (
+        <Task
+          key={index}
+          task={task}
+          onDelete={onDelete}
+          onToggle={onToggle}
+        />
       ))}
     </>
   );
